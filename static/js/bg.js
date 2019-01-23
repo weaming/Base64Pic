e566cb52bf3a490186407dca06a6ffe32c5ed4d5
weaming <garden.yuen@gmail.com>
2019-01-23 12:36:33 +0800

    add collectImage

--- static/js/bg.js
+++ static/js/bg.js
@@ -1,16 +1,20 @@
 // Google searchEngine
-chrome.storage.local.get('searchEngine', function(items) {
-  if (!items.searchEngine){
-    chrome.storage.local.set({'searchEngine': "https://gg.ruanjiadeng.com"}, function(){console.log('Saved: '+value)});
-  }
+chrome.storage.sync.get(['searchEngine'], function(settings) {
+    if (!settings.searchEngine) {
+        chrome.tabs.create({
+            'url': 'chrome://extensions/?options=' + chrome.runtime.id
+        });
+    }
 });
 
 chrome.omnibox.onInputEntered.addListener(omniboxFunc);
-function omniboxFunc(text){
-    chrome.storage.local.get('searchEngine', function(items){
-        window.open(items.searchEngine + "/search?q=" + text, "_blank");
+
+function omniboxFunc(text) {
+    chrome.storage.sync.get(['searchEngine'], function(settings) {
+        window.open(settings.searchEngine + "/search?q=" + text, "_blank");
     });
 }
+
 
 //Right click menu
 function getBase64Image(img) {
@@ -26,34 +30,92 @@ function getBase64Image(img) {
     return dataURL
 }
 
-function imageOnClick(info, tab) {
-  var img = document.createElement('img');
-  img.src = info.srcUrl;
-  img.onload = function() {
-    var data = getBase64Image(img);
-    var tmp = document.createElement("p")
-    document.body.appendChild(tmp);
-    tmp.innerHTML = data;
+function base64EncodeImage(info, tab) {
+    var img = document.createElement('img');
+    img.src = info.srcUrl;
+    img.onload = function() {
+        var data = getBase64Image(img);
+        var tmp = document.createElement("p")
+        document.body.appendChild(tmp);
+        tmp.innerHTML = data;
 
-    var range = document.createRange();
-    range.selectNode(tmp);
-    window.getSelection().addRange(range);
-    try {
-        // Now that we've selected the anchor text, execute the copy command
-        var result = document.execCommand('copy');
-        tmp.parentNode.removeChild(tmp);
-        var msg = result ? 'successful' : 'unsuccessful';
-        alert('Parse image ' + msg + '\n\n' + info.srcUrl);
-    } catch(err) {
-        console.log('Oops, unable to copy');
+        var range = document.createRange();
+        range.selectNode(tmp);
+        window.getSelection().addRange(range);
+        try {
+            // Now that we've selected the anchor text, execute the copy command
+            var result = document.execCommand('copy');
+            tmp.parentNode.removeChild(tmp);
+            var msg = result ? 'successful' : 'unsuccessful';
+            alert('Parse image ' + msg + '\n\n' + info.srcUrl);
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
     }
-  }
 }
 
-var createProperties = {
-    "title": "Base64编码当前图片",
-    "contexts": ["image"],
-    "onclick": imageOnClick
+function collectImage(info, tab) {
+    let url = info.srcUrl;
+    let API = 'https://api.drink.cafe/api/services/images.create';
+    chrome.storage.sync.get(['APICollectImage'], function(settings) {
+        if (settings.APICollectImage) {
+            API = settings.APICollectImage
+        }
+        doRequest(API, url)
+    })
 }
 
-var id = chrome.contextMenus.create(createProperties);
+function doRequest(API, url) {
+    fetch(API, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify({
+                "url": url,
+                "title": document.title,
+                "meta": {
+                    "via": "chrome extenion"
+                }
+            })
+        })
+        .then(res => res.json())
+        .then(reponse => {
+            chrome.notifications.create(
+                notificationId = url,
+                options = {
+                    "type": "basic",
+                    "title": "ToolsBox",
+                    "iconUrl": url,
+                    "message": `succeed collect ${url}`,
+                },
+                function() {
+                    if (chrome.runtime.lastError) {
+                        alert(chrome.runtime.lastError.message);
+                    }
+                })
+        })
+        .catch(error => console.log('Error:', error));
+}
+
+let menuProperties = [{
+        "title": "Base64编码当前图片",
+        "contexts": ["image"],
+        "onclick": base64EncodeImage
+    },
+    {
+        "title": "收藏这张图片",
+        "contexts": ["image"],
+        "onclick": collectImage,
+    },
+]
+
+menuProperties.forEach(
+    function(createProperties) {
+        chrome.contextMenus.create(createProperties);
+    }
+)
--- static/js/options.js
+++ static/js/options.js
@@ -1,15 +1,33 @@
-$("#saveSearch").click(saveEngine);
+// https://developer.chrome.com/extensions/storage
 
-function saveEngine(){
-  value = $("#searchEngine").val();
-  chrome.storage.local.set({'searchEngine':value}, function(){console.log('Saved: '+value)});
+function saveSettings() {
+    let settings = {
+        'searchEngine': $("#searchEngine").val(),
+        'APICollectImage': $("#APICollectImage").val(),
+    }
+    chrome.storage.sync.set(settings, function() {
+        console.log('Saved!', settings)
+    });
 }
 
-chrome.storage.local.get('searchEngine', function(items) {
-  if (!items.searchEngine){
-    $("#searchEngine").val("https://gg.ruanjiadeng.com");
-    saveEngine();
-  } else {
-    $("#searchEngine").val(items.searchEngine);
-  }
+$("#saveButton").click(saveSettings);
+
+chrome.storage.sync.get(['searchEngine', 'APICollectImage'], function(settings) {
+    let defaultAPICollectImage = 'https://api.drink.cafe/api/services/images.create';
+    let defaultSearchEngine = "https://g.bitsflow.org";
+    console.log('Got settings', settings)
+
+    if (!settings.APICollectImage) {
+        $("#APICollectImage").val(defaultAPICollectImage);
+    } else {
+        $("#APICollectImage").val(settings.APICollectImage);
+    }
+
+    if (!settings.searchEngine) {
+        $("#searchEngine").val(defaultSearchEngine);
+    } else {
+        $("#searchEngine").val(settings.searchEngine);
+    }
+
+    saveSettings();
 });
